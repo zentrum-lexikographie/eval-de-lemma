5626b41011c73beec59ae76132c798490d9c1fa5
LydiaKoerber <lydi.koerber@web.de>
2022-08-10 11:57:35 +0200

    retry archimob download

--- download-archimob.sh
+++ download-archimob.sh
@@ -6,6 +6,6 @@ DATAFOLDER=datasets
 wget -c https://drive.switch.ch/index.php/s/vYZv9sNKetuPYTn/download -O ${DATAFOLDER}/Archimob_Release_2.zip
 unzip -q ${DATAFOLDER}/Archimob_Release_2.zip -d ${DATAFOLDER}
 mkdir ${DATAFOLDER}/archimob
-mv ${DATAFOLDER}/Archimob_Release_2/Archimob_Release_2/* ${DATAFOLDER}/archimob
+mv ${DATAFOLDER}/Archimob_Release_2/Archimob_Release_2/*.xml ${DATAFOLDER}/archimob
 rm -r ${DATAFOLDER}/Archimob_Release_2
 rm ${DATAFOLDER}/Archimob_Release_2.zip
