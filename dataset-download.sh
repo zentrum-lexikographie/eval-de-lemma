ff36a9451b723ac3794123e7f885225935988ab1
LydiaKoerber <lydi.koerber@web.de>
2022-08-10 12:01:05 +0200

    correct download script

--- dataset-download.sh
+++ dataset-download.sh
@@ -38,11 +38,10 @@ rm -r ${DATAFOLDER}/germanc/{LING-COL,LING-GATE,RAW,TEI}
 rm ${DATAFOLDER}/germanc.zip
 
 # ArchiMob Release 2
-# TODO check on lal
 wget -c https://drive.switch.ch/index.php/s/vYZv9sNKetuPYTn/download -O ${DATAFOLDER}/Archimob_Release_2.zip
 unzip -q ${DATAFOLDER}/Archimob_Release_2.zip -d ${DATAFOLDER}
 mkdir ${DATAFOLDER}/archimob
-mv ${DATAFOLDER}/Archimob_Release_2/Archimob_Release_2/* ${DATAFOLDER}/archimob
+mv ${DATAFOLDER}/Archimob_Release_2/*.xml ${DATAFOLDER}/archimob
 rm -r ${DATAFOLDER}/Archimob_Release_2
 rm ${DATAFOLDER}/Archimob_Release_2.zip
 
