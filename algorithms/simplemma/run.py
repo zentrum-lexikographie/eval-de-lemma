0fac4f0bb877cd6ed0f4b962af5ef98eda3dacc1
LydiaKoerber <lydi.koerber@web.de>
2022-09-20 09:54:09 +0200

    create smaller data frames

--- algorithms/simplemma/run.py
+++ algorithms/simplemma/run.py
@@ -18,8 +18,6 @@ warnings.filterwarnings("ignore")
 
 # (A) Run all benchmarks
 results = []
-# store different lemmatizations
-df = pd.DataFrame(columns=['corpus', 'token', 'pos', 'lemma_gold', 'lemma_pred'])
 
 for x_test, y_test, z_test, dname in load_data(DATASETSPATH):
     try:
@@ -36,10 +34,12 @@ for x_test, y_test, z_test, dname in load_data(DATASETSPATH):
         tracemalloc.stop()
         y_pred = list(itertools.chain(*y_pred))
         x_test = list(itertools.chain(*x_test))
-        # output incorrect lemmatizations
+        # store and output different lemmatizations
+        df = pd.DataFrame(columns=['token', 'lemma_gold', 'lemma_pred'])
         for i in range(len(y_test)):
             if y_test[i] != y_pred[i]:
-                df.loc[i] = [dname, x_test[i], z_test[i], y_test[i], y_pred[i]]
+                df.loc[i] = [x_test[i], y_test[i], y_pred[i]]
+        df.to_csv(f"../../nbs/lemmata-simplemma-{dname}.csv")
         # (A.3) Compute metrics
         metrics = metrics_by_pos(y_test, y_pred, z_test)
         # Save results
@@ -55,5 +55,3 @@ for x_test, y_test, z_test, dname in load_data(DATASETSPATH):
 # store results
 with open("../../nbs/results-simplemma.json", "w") as fp:
     json.dump(results, fp, indent=4)
-# output mis-lemmatizations
-df.to_csv("../../nbs/lemmata-simplemma.csv")
