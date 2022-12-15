dcd41b7afee1f2961173cb29adddb35203c056b0
LydiaKoerber <lydi.koerber@web.de>
2022-12-15 16:55:16 +0100

    add pre-tagged stanza

--- algorithms/stanza/run.py
+++ algorithms/stanza/run.py
@@ -1,3 +1,4 @@
+import itertools
 import json
 import logging
 import stanza
@@ -48,4 +49,39 @@ for x_test, y_test, z_test, z_test_xpos, dname in load_data(DATASETSPATH):
 
 # store results
 with open("../../nbs/results-stanza.json", "w") as fp:
+    json.dump(results, fp, indent=4)
+
+
+# run with PoS tags in input
+# (A) Load stanza model
+model = stanza.Pipeline(
+    lang='de', processors='tokenize,lemma',
+    lemma_pretagged=True, tokenize_pretokenized=True)
+
+
+def predict(x_test, y_test, z_test):
+    lemmata = []
+    for j, sent in enumerate(x_test):  # lemmatize by sentence
+        doc = stanza.models.common.doc.Document([[{'id': i, 'text': token,
+                                                   'upos': z_test[j][i]} for i,
+                                                  token in enumerate(sent)]])
+        doc_lemmatised = model(doc)
+        lemmata += [[t.lemma for t in sent.words]
+                    for sent in doc_lemmatised.sentences]
+    return list(itertools.chain(*lemmata))  # flatten sequence
+
+
+# (B) Run all benchmarks
+results = []
+
+for x_test, y_test, z_test, z_test_xpos, dname in load_data(DATASETSPATH):
+    try:
+        results.append(run_algorithm(predict, x_test, y_test, z_test,
+                                     z_test_xpos, dname, 'stanza'))
+    except Exception as err:
+        logger.error(err)
+
+
+# store results
+with open("../../nbs/results-stanza-pretagged.json", "w") as fp:
     json.dump(results, fp, indent=4)
