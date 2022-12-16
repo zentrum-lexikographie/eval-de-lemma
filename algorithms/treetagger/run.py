4fa244ab61fa74b92bccad627e6f2c6bf15b4c62
LydiaKoerber <lydi.koerber@web.de>
2022-12-16 12:19:15 +0100

    add treetagger

--- algorithms/treetagger/run.py
+++ algorithms/treetagger/run.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import sys
+import treetaggerwrapper
 
 sys.path.append("../..")
 from src.loader import load_data
@@ -21,9 +22,13 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
+tagger = treetaggerwrapper.TreeTagger(TAGLANG='de')
+
+
 def predict(x_test, y_test, z_test):
-    # baseline lemmatization: lemma = token
-    return x_test
+    tags = tagger.tag_text(x_test)
+    # saved as strings e.g. "text\tNN\ttext"
+    return [entry.split('\t')[2] for entry in tags]
 
 
 # (A) Run all benchmarks
@@ -32,11 +37,11 @@ results = []
 for x_test, y_test, z_test, z_test_xpos, dname in load_data(DATASETSPATH):
     try:
         results.append(run_algorithm(predict, x_test, y_test, z_test,
-                                     z_test_xpos, dname, 'baseline'))
+                                     z_test_xpos, dname, 'treetagger'))
     except Exception as err:
         logger.error(err)
 
 
 # store results
-with open("../../nbs/results-baseline.json", "w") as fp:
+with open("../../nbs/results-treetagger.json", "w") as fp:
     json.dump(results, fp, indent=4)
