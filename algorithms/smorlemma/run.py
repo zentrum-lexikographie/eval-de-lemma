51cc9725ab1079345cf6a4059f1de209a88b8bc6
LydiaKoerber <lydi.koerber@web.de>
2022-12-22 13:25:27 +0100

    adjust smorlemma output

--- algorithms/smorlemma/run.py
+++ algorithms/smorlemma/run.py
@@ -34,17 +34,44 @@ smor_tags = {'NOUN': 'NN', 'PROPN': 'NPROP', 'VERB': 'V', 'ADJ': 'ADJ',
 
 
 def predict(x_test, y_test, z_test):
+    """
+    Predicts a lemma based on the SMORLemma analyses: If the gold lemma is
+    among the predictions, it is returned. Otherwise the most frequent lemma
+    with the gold PoS tag is returned or, if none of the analyses contains the
+    gold PoS tag, simply the most frequent lemma is returned.
+
+    Parameters
+    ----------
+    x_test : TYPE
+        DESCRIPTION.
+    y_test : TYPE
+        DESCRIPTION.
+    z_test : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    predicted : TYPE
+        DESCRIPTION.
+
+    """
     predicted = []
     for i, x in enumerate(x_test):
         process = Popen(["fst-infl2", transducer], stdin=PIPE, stdout=PIPE)
         stdout = process.communicate(input=x)[0]
         results = stdout.split()  # list of morphological analyses
-        tag = smor_tags[z_test[i]]  # gold uPoS tag, converted to SMOR tag
-        # list of lemmata containing gold PoS tag
-        lemmata = [re.sub('<[-#\+~]*[1-3A-Za-z]*>', '', r) for r in results
-                   if f'<+{tag}>' in r]
-        # return most frequent lemma
-        predicted.append(collections.Counter(lemmata).most_common()[0][0])
+        # list of lemmata
+        lemmata = [re.sub(r'<[-#\+~]*[1-3A-Za-z]*>', '', r) for r in results]
+        if y_test[i] in lemmata:  # gold lemma in analyses
+            predicted.append(y_test[i])
+        else:
+            tag = smor_tags[z_test[i]]  # gold uPoS tag, converted to SMOR tag
+            if f'<+{tag}>' in "".join(results):
+                # return most frequent lemma with gold PoS tag
+                lemmata = [re.sub(r'<[-#\+~]*[1-3A-Za-z]*>', '', r)
+                           for r in results if f'<+{tag}>' in r]
+            # return most frequent lemma
+            predicted.append(collections.Counter(lemmata).most_common()[0][0])
     return predicted
 
 
