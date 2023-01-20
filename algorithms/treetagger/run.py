eb26573596edf3cbca89e0cc7777b79f2f6518f9
LydiaKoerber <lydi.koerber@web.de>
2023-01-20 17:40:25 +0100

    add rnntagger

--- algorithms/treetagger/run.py
+++ algorithms/treetagger/run.py
@@ -28,7 +28,7 @@ def predict(x_test, y_test, z_test, z_test_xpos):
     with open("pretokenized.txt", "w") as fp:
         for sent in x_test:
             for token in sent:
-                fp.write(f'{token}\n')
+                fp.write(token + '\n')
     # call tree tagger without tokenization
     os.system("cd tagger && bin/tree-tagger -token -lemma -sgml -quiet -pt-with-lemma lib/german.par ../pretokenized.txt > ../tagged.tsv")
     output = pd.read_csv('tagged.tsv', sep='\t')  # lines: token, pos, lemma
