0b05c296b4779e8d4ba4b0ccf7c541c28b5c3f10
LydiaKoerber <lydi.koerber@web.de>
2023-01-23 09:14:17 +0100

    adjust rnntagger script

--- algorithms/rnntagger/run.py
+++ algorithms/rnntagger/run.py
@@ -29,6 +29,8 @@ def predict(x_test, y_test, z_test, z_test_xpos):
         for sent in x_test:
             for token in sent:
                 fp.write(token + '\n')
+            fp.write("\n")
+        fp.write("\n")
     # call rnn tagger without tokenization and pos tagging
     # pos tags
     os.system("cd RNNTagger && python3 './PyRNN/rnn-annotate.py' './lib/PyRNN/german' ../pretokenized.txt > ../tagged.txt")
