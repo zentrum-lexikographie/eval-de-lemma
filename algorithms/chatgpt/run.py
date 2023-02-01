769bd12268f5bcb6a6ff126ed68796c062e31e16
LydiaKoerber <lydi.koerber@web.de>
2023-02-01 14:17:01 +0100

    prepare turkuNLP installation

--- algorithms/chatgpt/run.py
+++ algorithms/chatgpt/run.py
@@ -24,8 +24,8 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
+# bash command: export OPEN_AI_KEY=INSERT_KEY_HERE
 openai.api_key = os.environ(["OPEN_AI_KEY"])
-# export OPEN_AI_KEY=whchwdkjhwjkh636338
 
 
 def predict(x_test, y_test, z_test, z_test_xpos):
