8ae312bdb65431d75aca165ab7b01857197e68c0
LydiaKoerber <lydi.koerber@web.de>
2023-02-02 12:05:53 +0100

    correct install script

--- algorithms/turkunlp/install.sh
+++ algorithms/turkunlp/install.sh
@@ -7,11 +7,11 @@ git checkout orig-parser-pre-2021
 git submodule update --init --recursive
 
 # install venv
-python3.7 -m venv ".venv"
-source ".venv/bin/activate"
+python3.7 -m venv .venv
+source .venv/bin/activate
 pip install --upgrade pip
 pip3 install wheel
 pip install torch==0.4.1 -f https://download.pytorch.org/whl/torch_stable.html
-pip install -r "../requirements.txt"
+pip install -r ../requirements.txt
 python3 fetch_models.py de_gsd
 cd ..
