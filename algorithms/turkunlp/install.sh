048d9ca05965a586a47a0324ec6c4b99f15d4bbf
LydiaKoerber <lydi.koerber@web.de>
2023-02-02 11:59:41 +0100

    correct install script

--- algorithms/turkunlp/install.sh
+++ algorithms/turkunlp/install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-SRCDIR=$(dirname "$0")
-
 # clone repo
 git clone https://github.com/TurkuNLP/Turku-neural-parser-pipeline.git
 cd Turku-neural-parser-pipeline
@@ -10,12 +8,12 @@ git submodule update --init --recursive
 cd ..
 
 # install venv
-python3.7 -m venv "${SRCDIR}/Turku-neural-parser-pipeline/.venv"
-source "${SRCDIR}/Turku-neural-parser-pipeline/.venv/bin/activate"
+python3.7 -m venv "Turku-neural-parser-pipeline/.venv"
+source "Turku-neural-parser-pipeline/.venv/bin/activate"
 pip install --upgrade pip
 pip3 install wheel
 pip install torch==0.4.1 -f https://download.pytorch.org/whl/torch_stable.html
-pip install -r "${SRCDIR}/requirements.txt"
+pip install -r "requirements.txt"
 cd Turku-neural-parser-pipeline
 python3 fetch_models.py de_gsd
 cd ..
