04b6ee1b76b156539961f16b46ad16c7a4ea3436
LydiaKoerber <lydi.koerber@web.de>
2023-02-22 18:55:48 +0100

    continue clean-up of gpt3 outputs

--- algorithms/chatgpt/run.py
+++ algorithms/chatgpt/run.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import json
 import logging
+import os
 import sys
 
 sys.path.append("../..")
@@ -24,10 +25,27 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-def predict(x_test, dname):
+def predict(x_test, y_test, z_test, z_test_xpos, dname):
     """Read lemmata from API query outputs."""
     lemmata = clean_up(f'../../nbs/chatgpt_outputs/chatgpt-{dname}.txt')
     # TODO prevent index errors
+    print(len(x_test), len(lemmata))
+    wrong = dict()
+    j = 0  # second index for removals
+    x_test2 = x_test
+    for i, sent in enumerate(x_test2):
+        print(j, i)
+        if j > len(lemmata)-1:  # end of predicted list reached
+            break
+        if len(sent) != len(lemmata[j]):
+            #print(sent, x_test[i])
+            wrong[str(j)] = (sent, lemmata[j])
+            # ignore sentence, remove from all arrays to ensure computations
+            del lemmata[j], x_test[j], y_test[j], z_test[j], z_test_xpos[j]
+        else:
+            j += 1
+    print(wrong)
+    print(len(wrong.items()))
     return lemmata
 
 
@@ -35,9 +53,10 @@ def predict(x_test, dname):
 results = []
 
 for x_test, y_test, z_test, z_test_xpos, dname in load_data(DATASETSPATH):
-    try:
-        results.append(run_algorithm(predict, x_test, y_test, z_test,
-                                     z_test_xpos, dname, 'chatgpt'))
+    try:  # not all datasets lemmatized with gpt-3
+        if os.path.exists(f"../../nbs/chatgpt_outputs/chatgpt-{dname}.txt"):
+            results.append(run_algorithm(predict, x_test, y_test, z_test,
+                                         z_test_xpos, dname, 'chatgpt'))
     except Exception as err:
         logger.error(err)
 
