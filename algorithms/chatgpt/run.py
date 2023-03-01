4282b17996a5097019d6afa9bf0ba6f34ad40570
LydiaKoerber <lydi.koerber@web.de>
2023-03-01 19:03:08 +0100

    debug chatgpt alignment, switch to full output

--- algorithms/chatgpt/run.py
+++ algorithms/chatgpt/run.py
@@ -31,37 +31,40 @@ warnings.filterwarnings("ignore")
 def predict(x_test, y_test, z_test, z_test_xpos, dname):
     """Read lemmata from API query outputs, resolve alignment issues."""
     lemmata = clean_up(f'../../nbs/chatgpt_outputs/chatgpt-{dname}.txt')
-    keep_sents = []  # indices of kept sentences in gold data
-    keep_sents_lem = []  # indices of kept sentences in predicted lemma list
+    keep_sents = []  # indices of kept sentences in in gold data
+    keep_sents_lem = []  # indices of kept sentences predicted lemma list
     wrong = dict()
-    if len(x_test) == len(lemmata):  # same number of sentences in pred & gold
-        for i, sent in enumerate(x_test):
-            if len(sent) != len(lemmata[i]):
-                wrong[str(i)] = (sent, lemmata[i])
-            else:
-                keep_sents.append(i)
+    j = 0  # index in token list
+    for i, sent in enumerate(lemmata):
+        if i > len(x_test)-1 or j > len(x_test)-1:  # end of token list reached
+            print('yay')
+            break
+        if len(sent) != len(x_test[j]):
+            # alignment issues:
+            if len(sent) == len(x_test[j-1]):  # check previous
+                keep_sents.append(j-1)
                 keep_sents_lem.append(i)
-    else:  # different number of sentences in pred & gold
-        j = 0  # index in predictions list
-        for i, sent in enumerate(x_test):
-            if i > len(lemmata)-1:  # end of predicted list reached
-                break
-            if len(sent) != len(lemmata[j]):
-                # alignment issues:
-                if len(sent) == len(lemmata[j-1]):  # check previous
-                    keep_sents.append(i)
-                    keep_sents_lem.append(j-1)
-                elif len(sent) == len(lemmata[j+1]):  # check next
-                    keep_sents.append(i)
-                    keep_sents_lem.append(j+1)
-                    j += 2
-                else:  # different sentence length
-                    wrong[str(i)] = (sent, lemmata[j])
-                    j += 1
-            else:
-                keep_sents.append(i)
-                keep_sents_lem.append(j)
+                continue
+            elif len(sent) == len(x_test[j+1]):  # check next
+                keep_sents.append(j+1)
+                keep_sents_lem.append(i)
+                j += 2
+                continue
+            elif len(sent) == len(x_test[j+2]):  # check after next
+                keep_sents.append(j+2)
+                keep_sents_lem.append(i)
+                j += 3
+            elif len(sent) == len(x_test[j-2]):  # check before previous
+                keep_sents.append(j-2)
+                keep_sents_lem.append(i)
+                j -= 1
+            else:  # different sentence length
+                wrong[str(i)] = (sent, x_test[j])
                 j += 1
+        else:
+            keep_sents.append(j)
+            keep_sents_lem.append(i)
+            j += 1
     assert len(keep_sents) == len(keep_sents_lem)
     return [lemmata[j] for j in keep_sents_lem], \
         [x_test[j] for j in keep_sents], [y_test[j] for j in keep_sents], \
@@ -78,6 +81,9 @@ for x_test, y_test, z_test, z_test_xpos, dname in load_data(DATASETSPATH):
             y_pred, x_test_eval, y_test_eval, z_test_eval, z_test_xpos_eval = \
                 predict(x_test, y_test, z_test, z_test_xpos, dname)
             print(len(y_pred), len(x_test_eval))
+            # number of ignored sentences
+            num_ignored_sents = len(x_test) - len(y_pred)
+            num_sents = len(x_test)
             # flatten sequences
             y_test_eval = list(itertools.chain(*y_test_eval))
             z_test_xpos_eval = list(itertools.chain(*z_test_xpos_eval))
@@ -86,10 +92,7 @@ for x_test, y_test, z_test, z_test_xpos, dname in load_data(DATASETSPATH):
             z_test_eval = list(itertools.chain(*z_test_eval))
             # store and output lemmatizations of first 2000 tokens
             df = []
-            j = 2000
-            if len(y_pred) < j:
-                j = len(y_pred)
-            for i in range(j):
+            for i in range(len(y_pred)):
                 # dataframe with token, upos tag, xpos tag, gold lemma, pred
                 df.append([x_test_eval[i], z_test_eval[i], z_test_xpos_eval[i],
                            y_test_eval[i], y_pred[i]])
@@ -111,7 +114,9 @@ for x_test, y_test, z_test, z_test_xpos, dname in load_data(DATASETSPATH):
                 'dataset': dname, 'sample-size': size,
                 'lemmatizer': 'chatgpt', 'metrics': metrics,
                 'memory_current': 0,
-                'memory_peak': 0})
+                'memory_peak': 0,
+                'num_sents_corpus': num_sents,
+                'num_sents_ignored': num_ignored_sents})
     except Exception as err:
         logger.error(err)
 
